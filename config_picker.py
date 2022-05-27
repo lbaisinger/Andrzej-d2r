1500d7be65c43306905259c3d0c5a11ac5c35cdf
dt <michal.politanski@mgr-is.de>
2022-05-27 16:08:09 +0200

    updated config-picker with cave

--- config_picker.py
+++ config_picker.py
@@ -3,3 +3,5 @@ import importlib
 confname = 'laptop_bajs'
 modulename = ('player_configs.config_' + confname)
 config = importlib.import_module('player_configs.config_' + confname)
+
+from caves.any_8 import *
