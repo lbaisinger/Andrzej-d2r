1694fd7def0b8004b712feb7e0921cfcce3a498e
Michal Politanski <michal.politanski@mgr-is.de>
2022-05-28 17:45:36 +0200

    updated cave desc

--- caves/zolwie.py
+++ caves/zolwie.py
@@ -14,7 +14,7 @@ cave_name = "zolwie"
 wp_index = 1
 
 # All waypoints to loop in that cave
-# ustawilem sobie rope/shovel na 2 wp bo trudna mapa, jaskinia do ew. optymalizacji
+# Note: ze scyzorykiem to jeden chuj czy shovel czy rope
 wps = {1: 'shovel',
        2: 'shovel',
        3: 'shovel',
@@ -28,13 +28,9 @@ wps = {1: 'shovel',
 wp_val = list(wps.values())
 
 # Path to cave
-# from venore east bank
-# 4 na powierzchni
 to_cave_wps = {}
 
 # Path to depo
-# last one is venore east bank
-# from any in that cave
 to_dp_wps = {}
 
 # Monster list for the hunt, must match names of the .png files LOWERCASE, prioritize
