3d87ae678dd3555004633588624d6c1639bafcc2
Michal Politanski <michal.politanski@mgr-is.de>
2022-05-28 17:54:17 +0200

    cosmetics

--- cave.py
+++ cave.py
@@ -28,18 +28,16 @@ class Cave:
 
         return wrap
 
+    @timing()
     def use_rope(self):
         # works ok
-        timestamp = datetime.datetime.now()
         sleep(0.15)
         pyautogui.press(config.hotkey_rope)
-        sleep(0.15) #otherwise its too fast
+        sleep(0.15) # otherwise its too fast
         pyautogui.click(config.character)
-        timestamp2 = datetime.datetime.now()
-        looptime = timestamp2 - timestamp
-        print('TIME use_rope', looptime)
         return True
 
+    @timing()
     def use_shovel(self):
         # works ok
         timestamp = datetime.datetime.now()
@@ -47,12 +45,13 @@ class Cave:
         pyautogui.press(config.hotkey_shovel)
         sleep(0.15)
         pyautogui.click(config.character)
-        sleep(0.15) #otherwise its too fast
+        sleep(0.15) # otherwise its too fast
         timestamp2 = datetime.datetime.now()
         looptime = timestamp2 - timestamp
         print('TIME use_shovel', looptime)
         return True
 
+    @timing()
     def use_ladder(self):
         timestamp = datetime.datetime.now()
         sleep(0.1)
@@ -62,8 +61,97 @@ class Cave:
         print('TIME use_ladder', looptime)
         return True
 
+    # @timing
+    def is_on_wp(self, wp):
+        # todo minimap center coords
+        if self.utils.andrzej_szuka(region=config.minimap_center_cv,
+                                    image_path='./src/wp/' + str(wp) + '.png',
+                                    confidence=.7) is not False:
+            #print('is on wp')
+            return True
+        else:
+            #print('not on wp')
+            return False
+
+    # @timing
+    def do_go_wp(self, wp):
+
+        print('going to wp', wp)
+        try:
+            x, y = self.utils.andrzej_szuka(region=config.minimap_cv,
+                                           image_path='./src/wp/' + str(wp) + '.png',
+                                           confidence=0.69)
+            #print(x,y)
+            pyautogui.click(x+3, y+3)
+            pyautogui.moveTo(config.default)
+        except TypeError:
+            print('Couldnt find wp', wp)
+            return False
+
+    @timing()
+    def is_wp_fancy(self, wp, specials: {}):
+        # todo verify if rly works
+        timestamp = datetime.datetime.now()
+        # sprawdz czy podany wp specjalny
+        if specials[wp] in ['rope', 'shovel', 'ladder', 'lvl_changing_wp']:
+            # to bedzie specjalyn wp
+            print('am special')
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME is_wp_fancy T', looptime)
+            return True
+        else:
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME is_wp_fancy F', looptime)
+            return False
+
+    @timing()
+    def do_go_wp_plus(self, wp, specials: {}):
+        # todo timestamps
+        timestamp = datetime.datetime.now()
+        if specials[wp] == 'rope':
+            self.use_rope()
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME do_go_wp_plus rope', looptime)
+            return True
+        if specials[wp] == 'shovel':
+            self.use_shovel()
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME do_go_wp_plus shovel', looptime)
+            return True
+        if specials[wp] == 'ladder':
+            self.use_ladder()
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME do_go_wp_plus use_ladder', looptime)
+            return True
+        # todo test this boi
+        if specials[wp] == 'lvl_changing_wp':
+            if self.is_wp_in_range(wp+1):
+                return wp+1
+
+        # if did not catch in any of ifs above
+        return False
+
+    @timing()
+    def is_wp_in_range(self, wp):
+        try:
+            self.utils.andrzej_szuka(region=config.minimap_cv,
+                                     image_path='./src/wp/' + str(wp) + '.png',
+                                     confidence=0.69)
+            return True
+        except TypeError:
+            return False
+
+#
+# todo needs rework
+#
     def is_has_cap(self):
         # todo fix it to be sure that it can always read cap
+        # todo atm it happens that it sometimes print incorret values
         timestamp = datetime.datetime.now()
         bp = Backpack()
         try:
@@ -98,17 +186,67 @@ class Cave:
             print('TIME is_ready_to_go_dp T', looptime)
             return True
 
-    # @timing
-    def is_on_wp(self, wp):
-        # todo minimap center coords
-        if self.utils.andrzej_szuka(region=config.minimap_center_cv,
-                                    image_path='./src/wp/' + str(wp) + '.png',
-                                    confidence=.7) is not False:
-            #print('is on wp')
+
+#
+# Legacy bullshit, should be safe to remove
+#
+    def is_wp_in_range_legacy(self, wp):
+        wp_cords = pyautogui.locateCenterOnScreen("src/wp/" + str(wp) + ".png", region=config.minimap, confidence=.8)
+        if wp_cords is not None:
             return True
         else:
-            #print('not on wp')
             return False
+
+    def go_somewhere_legacy(self, wp, specials: {}):
+        nextwp = wp + 1
+
+        # sprawdz czy nie wszedl po shcodach/wszedl w dziure
+        if specials[wp] == 'lvl_changing_wp':
+            print('to level changer')
+            if self.is_wp_in_range(nextwp):
+                return nextwp
+
+        # sprawdz czy stoje na wp      
+        if self.is_on_wp(wp=wp, wp_val=list(specials.values())):
+            # sprawdz czy to nie koniec
+            if specials[wp] == 'LAST':
+                print('done! im there')
+                return True
+
+            # jak fancy to zrob cos 
+            if self.is_wp_fancy(wp=wp, specials=specials):
+                self.do_go_wp_plus(wp, specials)
+            # i dawaj na next
+            return nextwp
+
+            # w kazdym innym wypadku
+        self.do_go_wp(wp)
+        return wp
+
+    def do_go_wp_legacy(self, wp):
+        # szuka wp na mapie i go naciska
+        # dziala ok
+        timestamp = datetime.datetime.now()
+        img = PIL.Image.open("src/wp/" + str(wp) + ".png")
+        img_size = img.size
+        wp_img = img.resize((img_size[0] * config.scale,
+                             img_size[1] * config.scale))
+        wp_coord = pyautogui.locateCenterOnScreen(wp_img,
+                                                  region=config.minimap,
+                                                  confidence=.75)
+        if wp_coord is not None:
+            # print('going wp', str(wp), wp_coord[0], wp_coord[1])
+            pyautogui.click(wp_coord[0], wp_coord[1])
+            pyautogui.moveTo(config.default)  # Added moveto default (otherwise sometime mouse stays on wp and cannot detect)
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('{:<30} {:<20.2f}'.format('TIME GO TO WP:', looptime.total_seconds()))
+            return True
+        else:
+            print('couldnt find wp', wp)
+            timestamp2 = datetime.datetime.now()
+            looptime = timestamp2 - timestamp
+            print('TIME GP_WP NOT_OK', looptime)
 
     # @timing
     def is_on_wp_legacy(self, wp):
@@ -145,125 +283,3 @@ class Cave:
             # #try to move minimap
             # #add timestamp
             return False
-
-    # @timing
-    def do_go_wp(self, wp):
-
-        print('going to wp', wp)
-        try:
-            x,y = self.utils.andrzej_szuka(region=config.minimap_cv,
-                                           image_path='./src/wp/' + str(wp) + '.png',
-                                           confidence=0.69)
-            #print(x,y)
-            pyautogui.click(x+3,y+3)
-            pyautogui.moveTo(config.default)
-        except TypeError:
-            print('Couldnt find wp', wp)
-            return False
-
-
-    # @timing
-    def do_go_wp_legacy(self, wp):
-        # szuka wp na mapie i go naciska
-        # dziala ok
-        timestamp = datetime.datetime.now()
-        img = PIL.Image.open("src/wp/" + str(wp) + ".png")
-        img_size = img.size
-        wp_img = img.resize((img_size[0] * config.scale,
-                             img_size[1] * config.scale))
-        wp_coord = pyautogui.locateCenterOnScreen(wp_img,
-                                                  region=config.minimap,
-                                                  confidence=.75)
-        if wp_coord is not None:
-            # print('going wp', str(wp), wp_coord[0], wp_coord[1])
-            pyautogui.click(wp_coord[0], wp_coord[1])
-            pyautogui.moveTo(
-                config.default)  # Added moveto default (otherwise sometime mouse stays on wp and cannot detect)
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('{:<30} {:<20.2f}'.format('TIME GO TO WP:', looptime.total_seconds()))
-            return True
-        else:
-            print('couldnt find wp', wp)
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME GP_WP NOT_OK', looptime)
-
-    def is_wp_fancy(self, wp, specials: {}):
-        # todo verify if rly works
-        timestamp = datetime.datetime.now()
-        # sprawdz czy podany wp specjalny
-        if specials[wp] in ['rope', 'shovel', 'ladder']:
-            # to bedzie specjalyn wp
-            print('am special')
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME is_wp_fancy T', looptime)
-            return True
-        else:
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME is_wp_fancy F', looptime)
-            return False
-
-    def do_go_wp_plus(self, wp, specials: {}):
-        # todo timestamps
-        timestamp = datetime.datetime.now()
-        if specials[wp] == 'rope':
-            self.use_rope()
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME do_go_wp_plus rope', looptime)
-            return True
-        if specials[wp] == 'shovel':
-            self.use_shovel()
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME do_go_wp_plus shovel', looptime)
-            return True
-        if specials[wp] == 'ladder':
-            self.use_ladder()
-            timestamp2 = datetime.datetime.now()
-            looptime = timestamp2 - timestamp
-            print('TIME do_go_wp_plus use_ladder', looptime)
-            return True
-        #        if specials[wp] is None:
-        #            timestamp2 = datetime.datetime.now()
-        #            looptime = timestamp2 - timestamp
-        #            print('TIME do_go_wp_plus None', looptime)
-        #            return True
-        # if did not catch in any of ifs above
-        return False
-
-    def is_wp_in_range(self, wp):
-        wp_cords = pyautogui.locateCenterOnScreen("src/wp/" + str(wp) + ".png", region=config.minimap, confidence=.8)
-        if wp_cords is not None:
-            return True
-        else:
-            return False
-
-    def go_somewhere(self, wp, specials: {}):
-        nextwp = wp + 1
-
-        # sprawdz czy nie wszedl po shcodach/wszedl w dziure
-        if specials[wp] == 'lvl_changing_wp':
-            print('to level changer')
-            if self.is_wp_in_range(nextwp):
-                return nextwp
-
-        # sprawdz czy stoje na wp      
-        if self.is_on_wp(wp=wp, wp_val=list(specials.values())):
-            # sprawdz czy to nie koniec
-            if specials[wp] == 'LAST':
-                print('done! im there')
-                return True
-
-            # jak fancy to zrob cos 
-            if self.is_wp_fancy(wp=wp, specials=specials):
-                self.do_go_wp_plus(wp, specials)
-            # i dawaj na next
-            return nextwp
-
-            # w kazdym innym wypadku
-        self.do_go_wp(wp)
-        return wp
