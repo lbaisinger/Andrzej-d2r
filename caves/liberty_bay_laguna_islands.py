b2351d822dbe83a1a7dbee8c4d90076b2901f021
Michal Politanski <michal.politanski@mgr-is.de>
2022-06-12 01:26:53 +0200

    laguna islands waypoints

---
+++ caves/liberty_bay_laguna_islands.py
@@ -0,0 +1,46 @@
+##################
+### Cavebot config
+###
+### Liberty Bay
+### Laguna Islands
+###
+### Hall of fame:
+###
+###
+
+cave_name = "Liberty bay Laguna islands"
+### Waypoints
+
+# start waypoint
+wp_index = 1
+
+# All waypoints to loop in that cave
+wps = {1: None,
+       2: None,
+       3: None,
+       4: None,
+       5: None,
+       6: 'turtle',
+       7: None,
+       8: None,
+       9: None,
+       10: None,
+       11: 'turtle',
+       12: None,
+       13: None,
+       14: None,
+       15: None,
+       16: None,
+       17: 'turtle',
+       18: None,
+       19: None
+       }
+
+# Path to cave
+to_cave_wps = {}
+
+# Path to depo
+to_dp_wps = {}
+
+# Monster list for the hunt, must match names of the .png files LOWERCASE, prioritize
+target_list = ["any"]
