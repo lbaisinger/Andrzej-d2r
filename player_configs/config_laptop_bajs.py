5bcd7f5bfe0c5566c45cae1e821e09240c765da9
bajsi <lbaisinger@outlook.com>
2022-06-15 22:32:37 +0200

    config updt

--- player_configs/config_laptop_bajs.py
+++ player_configs/config_laptop_bajs.py
@@ -13,6 +13,29 @@ hp_pool_exura_cv = (820, 30, 845, 50)
 mana_pool_potek_cv = (980, 30, 1010, 50)
 burn_mana_cv = (890, 30, 915, 50)
 bw_cv = (1770, 355, 1820, 365)
+bw_2nd_cv = (1770, 370, 1820, 400)
+bw_full = (1760, 345, 1820, 475)
+# redbox_cv = (1740, 350, 1780, 470)
+redbox_cv = (1740, 350, 1780, 370)
+minimap_center_cv = (1800, 83, 1812, 95)
+minimap_cv = (1745, 35, 1865, 155)
+ring_cv = (1745, 230, 1790, 275)
+amulet_cv = (1745, 155, 1790, 200)
+
+################################
+#           BATTLE             #
+################################
+use_ring = True
+use_amulet = False
+pg_mode = True
+bloodrage = True
+exeta = True
+rush = True
+status_check = False
+paralyze_check = False
+poison_check = False
+rotation = ['r', 'f', 'r', 'g']
+
 ################################
 #           HEAL & MANA        #
 ################################
