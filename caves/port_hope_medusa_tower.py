c79a00fb2fa35b871dadf1216a723250785ad648
bajsi <lbaisinger@outlook.com>
2022-07-18 22:21:31 +0200

    - README.md - updated: fixed table width, v2.1 - readme.md - in progress: added nightmare cave, ... to caves working on next - added port_hope_medusa_tower.py WP

---
+++ caves/port_hope_medusa_tower.py
@@ -0,0 +1,48 @@
+##################
+### Cavebot config
+###
+### Any 16 wp
+###
+###
+### Hall of fame:
+### Worril:
+
+cave_name = "Any 16"
+### Waypoints
+
+# start waypoint
+wp_index = 1
+
+# All waypoints to loop in that cave
+wps = {1: 'lvl_changing_wp',
+       2: None,
+       3: 'lvl_changing_wp',
+       4: None,
+       5: 'lvl_changing_wp',
+       6: None,
+       7: 'lvl_changing_wp',
+       8: None,
+       9: 'lvl_changing_wp',
+       10: 'lvl_changing_wp',
+       11: None,
+       12: 'lvl_changing_wp',
+       13: None,
+       14: 'lvl_changing_wp',
+       15: None}
+wp_val = list(wps.values())
+
+# Path to cave
+# from venore east bank
+# 4 na powierzchni
+to_cave_wps = {}
+
+# Path to depo
+# last one is venore east bank
+# from any in that cave
+to_dp_wps = {}
+
+# Monster list for the hunt, must match names of the .png files LOWERCASE, prioritize
+target_list = ["any"]
+
+# FACC Backapck trash
+item_blacklsit = ['torch']
