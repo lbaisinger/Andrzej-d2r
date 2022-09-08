dbdfb2f92572942f76fa9ec0e53792d111ea47b9
dt <michal.politanski@mgr-is.de>
2022-09-08 22:08:44 +0200

    puchal_lapek update

--- player_configs/config_puchal_lapek.py
+++ player_configs/config_puchal_lapek.py
@@ -10,38 +10,38 @@ sqm_edge_length_px = 36
 # (y /2 ) + 24
 
 # hp/mana bar
-mana_pool_potek_cv = (1022, 30, 1022+32, 30+32)
-hp_pool_exura_cv = (898, 30, 898+32, 30+32)
-hp_pool_potek_cv = (699, 30, 699+32, 30+32)
-burn_mana_cv = (968, 30, 968,+32, 30+32)
+hp_pool_potek_cv = (699, 25, 699+20, 25+20)
+mana_pool_potek_cv = (1022, 25, 1022+20, 25+20)
+hp_pool_exura_cv = (898, 25, 898+20, 25+20)
+burn_mana_cv = (968, 25, 968+20, 25+20)
 
 # battle window
 #bw_cv = (4572, 887, 4572+150, 887+130)
-bw_cv = (1746, 468, 1746+75, 468+65)
+bw_cv = (1746, 462, 1746+75, 462+65)
 #bw_2nd_cv = (4572, 928, 4572+150, 928+130)
 bw_2nd_cv = (1746, 488, 1746+75, 488+65)
 #bw_full = (4572, 887, 4572+150, 887+130) # 130
-bw_full = (1746, 468, 1746+75, 468+65) # 130
+bw_full = (1746, 462, 1746+75, 462+65) # 130
 #redbox_cv = (4567, 882, 4567+50, 882+150)
-redbox_cv = (1744, 465, 1744+25, 465+75)
+redbox_cv = (1744, 462, 1744+40, 462+170)
 #redbox_cv_1st = (4567, 882, 4567+50, 882+50)
-redbox_cv_1st = (1744, 465, 1744+25, 465+75)
+redbox_cv_1st = (1744, 462, 1744+25, 462+75)
 
 # minimap
 #minimap_center_cv = (4675, 115, 4675+20, 120+15)
-minimap_center_cv = (1798, 82, 1798+20, 82+20)
+minimap_center_cv = (1795, 74, 1795+20, 74+20)
 
 # char items
 #status_bar = (4580, 535, 4580+110, 535+25)
 status_bar = (1750, 292, 1750+110, 292+25)
 
 #minimap_cv = (4580, 21, 4580+225, 21+210)
-minimap_cv = (1750, 35, 1750+120, 35+120)
+minimap_cv = (1750, 35, 1750+100, 35+100)
 
 #ring_cv = (4583, 424, 4583+70, 424+70) # 40 35
-ring_cv = (1752, 236, 1752+40, 236+35)
+ring_cv = (1750, 255, 1750+35, 255+35)
 #amulet_cv = (4598, 440, 4598+70, 440+70) # 40 40
-amulet_cv = (1759, 244, 1759+40, 244+40)
+amulet_cv = (1750, 244, 1750+40, 244+40)
 
 
 ################################
@@ -78,7 +78,7 @@ rush = True
 ################################
 #           HEAL & MANA `      #
 ################################
-scale = 2
+scale = 1
 # heal if hp low with poton
 hplow = True
 # heal if hp moderate with exura
@@ -89,7 +89,7 @@ manalow = True
 manahigh = False
 
 ################################
-#           HOTKEYS     `      #
+#           HOTKEYS            #
 ################################
 hotkey_ring = 'f10'
 hotkey_amulet = 'f9'
@@ -122,6 +122,6 @@ cap_region = (4737, 510, 40, 20)
 hotkey_hi = 'f10'
 hotkey_deposit_all = 'f11'
 hotkey_yes = 'f12'
-is_co_bic_custom_confidence = 0.999
+is_co_bic_custom_confidence = 0.99
 is_bije_custom_confidence = 0.25
 
